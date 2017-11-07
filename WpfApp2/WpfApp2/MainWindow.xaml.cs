591e9b175c295cf400d5efc60e9bf36a920747df
pr-yanka <yan0inf@gmail.com>
2017-11-07 13:12:00 +0200

    Mahapps!
    UPDATE: now we are using mahapps.

--- WpfApp2/WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace WpfApp2
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public NavigationController Controller { get; }
         public ViewModelFullMenu CurrentNavigation { get; }
