22dd5248d849e090c3d57b3e90e03655b89a5d3a
pr-yanka <yan0inf@gmail.com>
2017-11-14 20:18:39 +0200

    ADD. First steps to use and understand IRepository pattern to use for working with DB.

---
+++ WpfApp2/WpfApp2/Db/Models/BPVHip/BPVRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.Db.Models.BPV
+{
+    public class BPVHipRepository : IRepository<BPVHipEntryFull>
+    {
+        private BPVHipContext db;
+
+        public BPVHipRepository(BPVHipContext context)
+        {
+            this.db = context;
+        }
+
+        public IEnumerable<BPVHipEntryFull> GetAll()
+        {
+            return db.BPVEntries;
+        }
+
+        public BPVHipEntryFull Get(int id)
+        {
+            return db.BPVEntries.Find(id);
+        }
+
+        public void Create(BPVHipEntryFull entry)
+        {
+            db.BPVEntries.Add(entry);
+        }
+
+        public void Update(BPVHipEntryFull book)
+        {
+            db.Entry(book).State = EntityState.Modified;
+        }
+
+        public void Delete(int id)
+        {
+            BPVHipEntryFull book = db.BPVEntries.Find(id);
+            if (book != null)
+                db.BPVEntries.Remove(book);
+        }
+    }
+}
