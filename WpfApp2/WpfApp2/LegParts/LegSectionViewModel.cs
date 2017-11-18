449378516cfae11285e028b4a943d8c133f8fa9c
pr-yanka <yan0inf@gmail.com>
2017-11-18 03:18:53 +0200

    ADD. Fields for LegSectionViewModel.

--- WpfApp2/WpfApp2/LegParts/LegSectionViewModel.cs
+++ WpfApp2/WpfApp2/LegParts/LegSectionViewModel.cs
@@ -4,11 +4,59 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WpfApp2.Db.Models;
 
 namespace WpfApp2.LegParts
 {
     public abstract class LegSectionViewModel{
         private int _listNumber;
         public int ListNumber { get; set; }
+
+        public List<BPVHipStructure> StructureSource = new List<BPVHipStructure>();
+        //all values
+
+        //selected value
+        private BPVHipStructure _selectedValue;
+        public BPVHipStructure SelectedValue { get; set; }
+
+        private bool _hasSize;
+        //has size
+        public bool HasSize { get; set; }
+
+        //has double size
+        private bool _hasDoubleSize;
+        public bool HasDoubleSize { get; set; }
+
+        //has first part
+        private bool _hasFirstPart;
+        public bool HasFirstPart { get; set; }
+
+        //has second part
+        private bool _hasSecondPart;
+        public bool HasSecondPart { get; set; }
+
+        private string _text1;
+        public string Text1 { get; set; }
+
+        private string _text2;
+        public string Text2 { get; set; }
+
+        private float _size;
+        public float Size { get; set; }
+
+        private float _size2;
+        public float Size2 { get; set; }
+
+        public string SizeToText {
+            get
+            {
+                if (!HasSize && !HasDoubleSize)
+                {
+                    return "";
+                }
+                else if (HasSize) return Size.ToString();
+                else return Size.ToString() + "*" + Size2.ToString();
+            }
+        }
     };
 }
