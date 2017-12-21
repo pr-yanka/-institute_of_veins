73bdb17af88a3bc309cff4a2cf56c0121e81dff1
pr-yanka <yan0inf@gmail.com>
2017-12-21 15:24:39 +0200

    ADD. Insert SPS.

--- WpfApp2/WpfApp2/SQL_QUERIES/insert_BPV.sql
+++ WpfApp2/WpfApp2/SQL_QUERIES/insert_BPV.sql
@@ -8,6 +8,8 @@ INSERT INTO метрика(название) VALUES ('мм');
 
 INSERT INTO метрика(название) VALUES ('');
 
+INSERT INTO метрика(название) VALUES ('см');
+
 select * from метрика;
 
 INSERT INTO вид_бпв_хода
