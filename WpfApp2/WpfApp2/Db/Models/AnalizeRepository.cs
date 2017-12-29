3335b6a5028efe19a92086e89855cb2bf1245bf9
Serraya <serg.gow@gmail.com>
2017-12-30 00:40:16 +0200

    ADD. Analize/AnalizeType model

---
+++ WpfApp2/WpfApp2/Db/Models/AnalizeRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.Db.Models
+{
+    [Table("анализы")]
+    public class Analize
+    {
+        [Key]
+        [Column("id")]
+        public int Id { set; get; }
+        [Column("тип_анализа")]
+        public int analyzeType { set; get; }
+        [Column("дата")]
+        public DateTime data { set; get; }
+        [Column("id_пациента")]
+        public int patientId { set; get; }
+        [Column("анализ")]
+        public Byte[] ImageByte { set; get; }
+    }
+    public class AnalizeRepository : Repository<Analize>
+    {
+
+
+        public AnalizeRepository(DbContext context) : base(context)
+        {
+
+        }
+
+    }
+
+}
---
+++ WpfApp2/WpfApp2/Db/Models/AnalizeTypeRepository.cs
@@ -0,0 +1,31 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace WpfApp2.Db.Models
+{
+    [Table("виды_анализа")]
+    public class AnalizeType
+    {
+        [Key]
+        [Column("id")]
+        public int Id { set; get; }
+        [Column("название")]
+        public string Str { set; get; }
+
+        public override string ToString()
+        {
+            return Str;
+        }
+    }
+    public class AnalizeTypeRepository : Repository<AnalizeType>
+    {
+        public AnalizeTypeRepository(DbContext context) : base(context)
+        {
+
+        }
+    }
+
+}
