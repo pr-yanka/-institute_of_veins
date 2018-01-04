9a911449c7a26b82a9d7eb19b972661beb345758
pr-yanka <yan0inf@gmail.com>
2018-01-04 12:01:07 +0200

    FIX. script according to new create db script.

--- WpfApp2/WpfApp2/SQL_QUERIES/insert_doctors.sql
+++ WpfApp2/WpfApp2/SQL_QUERIES/insert_doctors.sql
@@ -1,50 +1,11 @@
 RENAME TABLE категории TO виды_категорий;
 
-RENAME TABLE звания TO виды_научных_званий;
-
-RENAME TABLE специализации TO виды_специализаций;
-
 CREATE TABLE `категории` (
 	`id_категории` INTEGER NOT NULL,
 	`id_врача` INTEGER NOT NULL,
 	PRIMARY KEY (`id_категории`, `id_врача`)
 );
 
-CREATE TABLE `научные_звания` (
-	`id_звания` INTEGER NOT NULL,
-	`id_врача` INTEGER NOT NULL,
-	PRIMARY KEY (`id_звания`, `id_врача`)
-);
-
-CREATE TABLE `виды_специализаций` (
-	`id` INTEGER NOT NULL,
-	`название` VARCHAR(50)
-);
-
-CREATE TABLE `специализации` (
-	`id_специализации` INTEGER NOT NULL,
-	`id_врача` INTEGER NOT NULL,
-	PRIMARY KEY (`id_специализации`, `id_врача`)
-);
-
-SET FOREIGN_KEY_CHECKS=0; DROP TABLE `врачи`; SET FOREIGN_KEY_CHECKS=1;
-
-CREATE TABLE `врачи` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`имя` varchar(20) NOT NULL,
-	`фамилия` varchar(40) NOT NULL,
-	`отчество` varchar(40) NOT NULL,
-    `дополнительно` VARCHAR(100),
-	PRIMARY KEY (`id`)
-);
-
-ALTER TABLE `специализации` ADD CONSTRAINT `специализации_fk0` FOREIGN KEY (`id_врача`) REFERENCES `врачи`(`id`);
-ALTER TABLE `специализации` ADD CONSTRAINT `специализации_fk1` FOREIGN KEY (`id_специализации`) REFERENCES `виды_специализаций`(`id`);
-
-ALTER TABLE `научные_звания` ADD CONSTRAINT `научные_звания_fk0` FOREIGN KEY (`id_врача`) REFERENCES `врачи`(`id`);
-ALTER TABLE `научные_звания` ADD CONSTRAINT `научные_звания_fk1` FOREIGN KEY (`id_звания`) REFERENCES `виды_научных_званий`(`id`);
-
-ALTER TABLE `категории` ADD CONSTRAINT `категории_fk0` FOREIGN KEY (`id_врача`) REFERENCES `врачи`(`id`);
 ALTER TABLE `категории` ADD CONSTRAINT `категории_fk1` FOREIGN KEY (`id_категории`) REFERENCES `виды_категории`(`id`);
 
 INSERT into виды_категории
@@ -82,7 +43,7 @@ INSERT into виды_специализаций
 
 INSERT INTO врачи
 	(имя, фамилия, отчество, дополнительно)
-    VALUES ("Оксана", "Сергеевна", "Рябинская", "очень классная женщина");
+    VALUES ("Сергеевна", "Оксана", "Рябинская", "очень классная женщина");
 
 INSERT INTO врачи
 	(имя, фамилия, отчество, дополнительно)
