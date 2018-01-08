5f79ae99f1e12dc947930c9475e9d3658e9ba2b2
Serraya <serg.gow@gmail.com>
2018-01-08 17:53:21 +0200

    FIX.Alter patology

--- WpfApp2/WpfApp2/SQL_QUERIES/alter_patology.sql
+++ WpfApp2/WpfApp2/SQL_QUERIES/alter_patology.sql
@@ -3,3 +3,7 @@ ADD COLUMN `архивирована` TINYINT NULL AFTER `id_пациента`,
 ADD COLUMN `месяц_появления` DATE NULL AFTER `архивирована`,
 ADD COLUMN `год_появления` DATE NULL AFTER `месяц_появления`,
 ADD COLUMN `месяц_исчезнование` DATE NULL AFTER `год_появления`;
+
+
+ALTER TABLE `med_db`.`патологии` 
+ADD COLUMN `год_исчезнование` DATE NULL AFTER `месяц_исчезнование`;
