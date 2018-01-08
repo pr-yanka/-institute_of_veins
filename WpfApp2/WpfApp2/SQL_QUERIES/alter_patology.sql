b43b471bfc901ab451d8676a98abdf308a93e28f
Serraya <serg.gow@gmail.com>
2018-01-08 13:48:20 +0200

    ADD. patology script

---
+++ WpfApp2/WpfApp2/SQL_QUERIES/alter_patology.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `med_db`.`патологии` 
+ADD COLUMN `архивирована` TINYINT NULL AFTER `id_пациента`,
+ADD COLUMN `месяц_появления` DATE NULL AFTER `архивирована`,
+ADD COLUMN `год_появления` DATE NULL AFTER `месяц_появления`,
+ADD COLUMN `месяц_исчезнование` DATE NULL AFTER `год_появления`;
