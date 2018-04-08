8091acbe894e4d3999f2871d60eed4d79d4b704d
Serraya <serg.gow@gmail.com>
2018-04-09 01:19:00 +0300

    UPDATE. ой

--- WpfApp2/WpfApp2/ViewModels/ViewModelLogin.cs
+++ WpfApp2/WpfApp2/ViewModels/ViewModelLogin.cs
@@ -48,8 +48,8 @@ namespace WpfApp2.ViewModels
 
             foreach (var acc in Data.Accaunt.GetAll)
             {
-                Name = "Admin";
-                CheckSum = CalculateMD5Hash("123");
+                //Name = "Admin";
+                //CheckSum = CalculateMD5Hash("123");
                 if (Name == acc.Name && acc.isEnabled == true)
                 {
                     isUeserNameCorrect = true;
